1af021b1f988ac72164b510b1302827ce2139187
muumi <chrisrobin.soon@gmail.com>
2022-09-05 16:54:41 +0300

    feat: add tunnid command

--- src/commands/tunnid.ts
+++ src/commands/tunnid.ts
@@ -1,20 +1,38 @@
 import axios from "axios";
 import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { TahvelTunniplaan } from "../types/types";
+import moment from "moment";
+moment.locale("et");
 
 module.exports = {
       data: new SlashCommandBuilder()
             .setName("tunnid")
-            .setDescription("Tänane tunniplaan!"),
+            .setDescription("Vaata erinevate päevade tunniplaani!")
+            .addStringOption(option =>
+                  option
+                        .setName("paev")
+                        .setDescription("Mis päeva tunniplaani soovid vaadata!")
+                        .setRequired(true)
+                        .addChoices(
+                              { name: "Esmaspäev", value: "0" },
+                              { name: "Teisipäev", value: "1" },
+                              { name: "Kolmapäev", value: "2" },
+                              { name: "Neljapäev", value: "3" },
+                              { name: "Reede", value: "4" },
+                        )),
       async execute(interaction: CommandInteraction) {
             await interaction.deferReply();
+            // Ugly could do better but works so fuck it
             const today = new Date();
-            const data = await (await axios.get(encodeURI(`https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14?from=${today.toISOString()}&studentGroups=6932&thru=${today.toISOString()}`))).data as TahvelTunniplaan;
+            const diff = today.getDate() - today.getDay() + (today.getDay() == 0 ? -6 : 1);
+            const monday = new Date(today.setDate(diff));
+            const selectedDay = new Date(monday.setDate(monday.getDate() + Number(interaction.options.get("paev")?.value)));
 
+            const data = await (await axios.get(encodeURI(`https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14?from=${selectedDay.toISOString()}&studentGroups=6932&thru=${selectedDay.toISOString()}`))).data as TahvelTunniplaan;
             const events = async () => {
                   const events = [];
                   for (let i = 0; i < data.timetableEvents.length; i++) {
-                        if (new Date(data.timetableEvents[i].date).getDate() == today.getDate()) {
+                        if (new Date(data.timetableEvents[i].date).getDate() == selectedDay.getDate()) {
                               events.push({
                                     name: data.timetableEvents[i].nameEt,
                                     startTime: data.timetableEvents[i].timeStart,
@@ -32,14 +50,14 @@ module.exports = {
             };
 
             const tunniplaanEmbed = new EmbedBuilder()
-                  .setTitle(`Tänased tunnid: ${today.toDateString()}`);
+                  .setTitle(`${moment(selectedDay).format("dddd").charAt(0).toUpperCase() + moment(selectedDay).format("dddd").slice(1)} ${moment(selectedDay).format("Do MMM")}`);
             const loop = async (events: Array<{ name: string, startTime: string, endTime: string, teacher: string, class: string}>) => {
                   for (let i = 0; i < events.length; i++) {
                         tunniplaanEmbed.addFields(
                               {
                                     name: events[i].name,
                                     value: `${events[i].startTime} - ${events[i].endTime}\n${events[i].teacher}\n${events[i].class}`,
-                                    inline: false,
+                                    inline: true,
                               },
                         );
                   }
