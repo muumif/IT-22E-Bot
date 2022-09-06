ff1c53540ae5580cbaa78ff975a50190446ccfc6
muumi <chrisrobin.soon@gmail.com>
2022-09-06 20:36:17 +0300

    feat: add reminder to bot channel

--- src/reminder.ts
+++ src/reminder.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { EmbedBuilder, TextChannel } from "discord.js";
 import schedule from "node-schedule";
 import { client } from ".";
 import { TahvelTunniplaan } from "./types/types";
@@ -9,12 +10,25 @@ export const lessonChecker = schedule.scheduleJob("00 07 * * *", async function(
       data.timetableEvents.forEach(timetableEvent => {
             const [minutes, hours] = [Number(timetableEvent.timeStart.split(":")[1]), Number(timetableEvent.timeStart.split(":")[0])];
             schedule.scheduleJob(`${minutes - 5} ${hours} * * *`, async function() {
-                  const user = await client.users.fetch(process.env.USER_ID);
-                  user.send(`Tund: ${timetableEvent.nameEt} hakkab 5 minuti pärast!\nKlassis: ${timetableEvent.rooms[0].buildingCode}-${timetableEvent.rooms[0].roomCode}`);
+                  const channel = client.channels.cache.get("1015312371494944828") as TextChannel; // Prolly should make channel ID changeable via command
+                  const embed = new EmbedBuilder()
+                        .setTitle(`Tund: ${timetableEvent.nameEt}`)
+                        .setDescription(`Algab 5 minuti pärast!\n Klassis: ${timetableEvent.rooms[0].buildingCode}-${timetableEvent.rooms[0].roomCode}`);
+                  channel.send({ embeds: [embed] });
+            });
+            schedule.scheduleJob(`${minutes - 2} ${hours} * * *`, async function() {
+                  const channel = client.channels.cache.get("1015312371494944828") as TextChannel;
+                  const embed = new EmbedBuilder()
+                        .setTitle(`Tund: ${timetableEvent.nameEt}`)
+                        .setDescription(`Algab 2 minuti pärast!\n Klassis: ${timetableEvent.rooms[0].buildingCode}-${timetableEvent.rooms[0].roomCode}`);
+                  channel.send({ embeds: [embed] });
             });
             schedule.scheduleJob(`${minutes} ${hours} * * *`, async function() {
-                  const user = await client.users.fetch(process.env.USER_ID);
-                  user.send(`Tund: ${timetableEvent.nameEt} hakkas!\nKlassis: ${timetableEvent.rooms[0].buildingCode}-${timetableEvent.rooms[0].roomCode}`);
+                  const channel = client.channels.cache.get("1015312371494944828") as TextChannel;
+                  const embed = new EmbedBuilder()
+                        .setTitle(`Tund: ${timetableEvent.nameEt}`)
+                        .setDescription(`Algas!\n Klassis: ${timetableEvent.rooms[0].buildingCode}-${timetableEvent.rooms[0].roomCode}`);
+                  channel.send({ embeds: [embed] });
             });
       });
 });
